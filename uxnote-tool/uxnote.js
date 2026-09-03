d7edf0dc8e2454fa71298a276fbf5246729995a1
Xavier Francisco <xavier.n.francisco@gmail.com>
2026-09-03 19:14:28 +0000

    chore(release): cut 2.1.0
    A minor, not a patch: this release carries the demo page settings, whose
    changelog line moves out of 2.0.0 and into the new entry, since it lands
    after 2.0.0 rather than in it.

--- uxnote-tool/uxnote.js
+++ uxnote-tool/uxnote.js
@@ -3,7 +3,7 @@
  * Author: ninefortyonestudio (https://github.com/ninefortyonestudio)
  * Fork: https://github.com/Qu4tro/uxnote-fork
  * Upstream: https://github.com/ninefortyonestudio/uxnote
- * Version: v2.0.0
+ * Version: v2.1.0
  * License: MIT (see LICENSE)
  *
  * This fork stores the annotations on the server named by data-server-url,
